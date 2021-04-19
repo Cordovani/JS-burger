843dc4caeb243dfce2aa2bbe125dc1160771c337
Cordovani <lor.cordovani@gmail.com>
2021-04-19 16:18:10 +0200

    pseudo-codice javascript

---
+++ burger/js/liveScript.js
@@ -0,0 +1,27 @@
+//DICHIARAZIONE DELLE FUNZIONI 
+
+// scandisce la collezione passando un ID 
+
+// controlla se in una collezione ci sono elementi checked restituisce un booleano
+
+// funzione per il submit delle forms
+
+// funzione per il reset delle forms
+
+// applica lo sconto passando due interi numero e percenutale di sconto
+
+//CORPO DEL PROGRAMMA
+
+
+// accetto il submit e resetto
+
+
+// controllo gli ingredienti con il check
+
+
+// prendo i dati 
+
+
+// genero il prezzo 
+
+
